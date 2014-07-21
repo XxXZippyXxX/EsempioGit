163240f2f591c96ee12cc76545e466649588ecc2
Alessandra Smarra <radiosmarra@hotmail.it>
2014-07-21 18:49:08 +0200

    commit del model

--- EsempioGit/src/model/Utente.java
+++ EsempioGit/src/model/Utente.java
@@ -1,20 +1,40 @@
 package model;
-
+/**
+ * 
+ * @author alessandra.smarra
+ *
+ */
 public class Utente {
 	private String username;
 	private String password;
 	public Utente() {
 		super();
 	}
+	/**
+	 * 
+	 * @return
+	 */
 	public String getUsername() {
 		return username;
 	}
+	/**
+	 * 
+	 * @param username
+	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	/**
+	 * 
+	 * @return
+	 */
 	public String getPassword() {
 		return password;
 	}
+	/**
+	 * 
+	 * @param password
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
