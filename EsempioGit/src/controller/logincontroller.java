f9ea1460f49a6bb2a048a746e1aa181371db982c
emilio.giliberti <emilio.giliberti@10.20.28.136>
2014-07-22 10:00:32 +0200

    Fix controller 2.0

--- EsempioGit/src/controller/logincontroller.java
+++ EsempioGit/src/controller/logincontroller.java
@@ -1,34 +1,22 @@
 package controller;
 
-import javax.servlet.http.HttpServletRequest;
-
-import model.Utente;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
+import model.Utente;
 
 
 @Controller
 public class logincontroller
 {
 
-	@Autowired
-	private HttpServletRequest context;
-
 
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	@ModelAttribute("utente")
-	public String login(final ModelMap model)
+	public String login(@ModelAttribute("utente") Utente utente)
 	{
-		final Utente utente = new Utente();
-		model.addAttribute("utente", utente);
-
+		
 		System.out.println(utente.getUsername() + utente.getPassword());
 
 
