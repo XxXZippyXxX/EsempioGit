8be4919452e13bc7bfe5f2dbdad8b232a293f7b1
emilio.giliberti <emilio.giliberti@10.20.28.136>
2014-07-22 16:05:03 +0200

    Fix Controller

--- EsempioGit/src/controller/logincontroller.java
+++ EsempioGit/src/controller/logincontroller.java
@@ -1,5 +1,15 @@
 package controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Enumeration;
+import java.util.Properties;
+
 import model.Utente;
 
 import org.springframework.stereotype.Controller;
@@ -17,14 +27,38 @@ public class logincontroller
 
 
 	@RequestMapping( method = RequestMethod.POST)
-	protected String login(@ModelAttribute("utente") Utente utente)
+	protected String login(@ModelAttribute("utente") Utente utente) throws IOException
 	{
-		
-		System.out.println(utente.getUsername() + utente.getPassword());
+		try
+		{
+			final File file = new File("utenti.properties");
+			final FileInputStream fileInput = new FileInputStream(file);
+			final Properties properties = new Properties();
+			properties.load(fileInput);
+			fileInput.close();
+			String password=properties.getProperty(utente.getUsername());
+			if(password.equals(utente.getPassword()))
+				return "login";
+			else{
+				return "loginko";
+			}
 
-
-		return "login";
+			//               final Enumeration enuKeys = properties.keys();
+			//               while (enuKeys.hasMoreElements())
+			//               {
+			//                      final String key = (String) enuKeys.nextElement();
+			//                      final String value = properties.getProperty(key);
+			//                      System.out.println(key + ": " + value);
+			//               }
+		}
+		catch (final FileNotFoundException e)
+		{
+			return "loginko";
+		}
+		catch (final IOException e)
+		{
+			return "loginko";
+		}
 	}
-
 
 }
